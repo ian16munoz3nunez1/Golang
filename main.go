534544fe6330e14308f6d530f8a97cbd6a5527af
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-16 16:19:40 -0500

    2022-06-16_16:20 --> Variables (Multiples variables)

--- main.go
+++ main.go
@@ -3,7 +3,13 @@ package main
 import "fmt"
 
 func main() {
-    const cadena = "Hola mundo"
-    fmt.Println(cadena)
+    var (
+        a = 2
+        b = 4
+        c = 8
+        d = 16
+    )
+
+    fmt.Println(a, b, c, d)
 }
 
