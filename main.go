e776a1b9c1a40ad53dad4ce6299ad7075a09e8f5
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-16 18:01:40 -0500

    2022-06-16_18:01 --> Condiciones/Ciclos (for y while)

--- main.go
+++ main.go
@@ -3,22 +3,14 @@ package main
 import "fmt"
 
 func main() {
-    var temp int
+    for i := 1; i <= 16; i++ {
+        fmt.Println(i)
+    }
 
-    fmt.Print("Ingresa la temperatura: ")
-    fmt.Scan(&temp)
-
-    switch {
-        case temp < 0:
-            fmt.Println("Esta helado")
-        case temp >= 0 && temp < 16:
-            fmt.Println("Esta frio")
-        case temp >= 16 && temp < 25:
-            fmt.Println("La temperatura esta perfecta")
-        case temp >= 25 && temp < 32:
-            fmt.Println("Esta caliente")
-        default:
-            fmt.Println("Esta ardiendo")
+    i := 16
+    for i >= 1 {
+        fmt.Println(i)
+        i--
     }
 }
 
