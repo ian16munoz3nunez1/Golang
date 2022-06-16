28da76b1285aa60bcd5e0913e7c04e0e474fdc6e
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-16 17:43:05 -0500

    2022-06-16_17:42 --> Condiciones/Ciclos (if-else_if-else)

--- main.go
+++ main.go
@@ -3,16 +3,21 @@ package main
 import "fmt"
 
 func main() {
-    if true {
-        fmt.Println("Es verdadero")
-    } else {
-        fmt.Println("Es falso")
-    }
+    var temp int
 
-    if false {
-        fmt.Println("Es verdadero")
+    fmt.Print("Ingresa la temperatura: ")
+    fmt.Scan(&temp)
+
+    if temp < 0 {
+        fmt.Println("Esta helado")
+    } else if temp >= 0 && temp < 16 {
+        fmt.Println("Esta frio")
+    } else if temp >= 16 && temp < 25 {
+        fmt.Println("La temperatura esta perfecta")
+    } else if temp >= 25 && temp < 32 {
+        fmt.Println("Esta caliente")
     } else {
-        fmt.Println("Es falso")
+        fmt.Println("Esta ardiendo")
     }
 }
 
