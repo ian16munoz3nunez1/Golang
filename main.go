723915b4c897180fa7c5ba9ad5db0058273f3a86
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-16 16:38:57 -0500

    2022-06-16_16:38 --> Variables (Scanf, Scan, bufio.NewScanner())

--- main.go
+++ main.go
@@ -1,15 +1,25 @@
 package main
 
-import "fmt"
+import (
+    "bufio"
+    "fmt"
+    "os"
+)
 
 func main() {
-    var (
-        a = 2
-        b = 4
-        c = 8
-        d = 16
-    )
+    // var numero float64
+    scanner := bufio.NewScanner(os.Stdin)
+    var nombre string
 
-    fmt.Println(a, b, c, d)
+    // fmt.Print("Numero: ")
+    // fmt.Scanf("%f", &numero)
+    // fmt.Scan(&numero)
+    fmt.Print("Ingresa tu nombre: ")
+    scanner.Scan()
+    nombre = scanner.Text()
+
+    //multiplicacion := numero * 4
+    //fmt.Println("Multiplicacion:", multiplicacion)
+    fmt.Println("Hola", nombre)
 }
 
