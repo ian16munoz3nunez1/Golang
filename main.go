2ad13546e918ac3c42f603b5ac601c49f6d829f7
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-16 17:37:26 -0500

    2022-06-16_17:37 --> Condiciones/Ciclos (if-else)

--- main.go
+++ main.go
@@ -1,25 +1,18 @@
 package main
 
-import (
-    "bufio"
-    "fmt"
-    "os"
-)
+import "fmt"
 
 func main() {
-    // var numero float64
-    scanner := bufio.NewScanner(os.Stdin)
-    var nombre string
+    if true {
+        fmt.Println("Es verdadero")
+    } else {
+        fmt.Println("Es falso")
+    }
 
-    // fmt.Print("Numero: ")
-    // fmt.Scanf("%f", &numero)
-    // fmt.Scan(&numero)
-    fmt.Print("Ingresa tu nombre: ")
-    scanner.Scan()
-    nombre = scanner.Text()
-
-    //multiplicacion := numero * 4
-    //fmt.Println("Multiplicacion:", multiplicacion)
-    fmt.Println("Hola", nombre)
+    if false {
+        fmt.Println("Es verdadero")
+    } else {
+        fmt.Println("Es falso")
+    }
 }
 
