de5fc26a562128c2b7f631302e96571f3ef57250
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-16 16:08:10 -0500

    2022-06-16_16:07 --> Variables (Variables globales)

--- main.go
+++ main.go
@@ -2,17 +2,11 @@ package main
 
 import "fmt"
 
-func main() {
-    var cadena0 string
-    var cadena1 string = "Hola mundo 1"
-    var cadena2 = "Hola mundo 2"
-    cadena3 := "Hola mundo 3"
+var cadena = "Hola mundo"
+var entero int64
 
-    fmt.Println(cadena0)
-    fmt.Println(cadena1)
-    fmt.Println(cadena2)
-    fmt.Println(cadena3)
-    fmt.Println(true == true)
-    fmt.Println(true != true)
+func main() {
+    fmt.Println(cadena)
+    fmt.Println(entero)
 }
 
