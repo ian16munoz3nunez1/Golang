709d601279f04f9fcbc1a84115c0c1ce83566aee
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-16 23:37:18 -0500

    2022-06-16_23:36 --> Arreglos/Slices (slices0)

--- main.go
+++ main.go
@@ -3,9 +3,10 @@ package main
 import "fmt"
 
 func main() {
-    x := [...]int{1, 2, 3, 4, 5}
-    fmt.Println(len(x), cap(x))
-    for _, v := range x {
+    x := [...]int{1, 2, 3, 4, 5, 6, 7, 8}
+    s := x[0:4]
+    fmt.Println(len(s), cap(s))
+    for _, v := range s {
         fmt.Println(v)
     }
 }
