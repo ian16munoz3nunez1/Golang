cc40602c903264609911354a67cb8d2b1de951bd
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-19 01:34:05 -0500

    2022-06-19_01:34 --> Map/JSON (map)

--- main.go
+++ main.go
@@ -3,14 +3,12 @@ package main
 import "fmt"
 
 func main() {
-    s := make([]int, 5, 16)
+    materias := make(map[string]int)
+    materias["Programacion"] = 100
+    materias["Logica"] = 90
+    materias["Algoritmia"] = 95
+    materias["Calculo"] = 90
 
-    fmt.Println(len(s), cap(s))
-    s = append(s, 1, 2, 3, 4, 5)
-    fmt.Println(len(s), cap(s))
-
-    for _, v := range s {
-        fmt.Println(v)
-    }
+    fmt.Println(materias)
 }
 
