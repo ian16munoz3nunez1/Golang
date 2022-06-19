fb1ea0fc3722afcb1e32c0acd2899e4c2e041938
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-19 15:39:46 -0500

    2022-06-19_15:40 --> Paquetes basicos 'os' (crear un archivo)

--- main.go
+++ main.go
@@ -2,29 +2,17 @@ package main
 
 import (
     "fmt"
-    "strings"
+    "os"
 )
 
 func main() {
-    cadena := "Arduino-C-Cpp-CSS-Go-HTML-Java-JS-Latino-Python-Ruby"
+    archivo, err := os.Create("go.txt")
+    if err != nil {
+        fmt.Println(err)
+        return
+    }
+    defer archivo.Close()
 
-    fmt.Println(cadena)
-    fmt.Println(strings.Contains(cadena, "Go"))
-    fmt.Println(strings.Count(cadena, "C"))
-    fmt.Println(strings.HasPrefix(cadena, "Arduino"))
-    fmt.Println(strings.HasSuffix(cadena, "Ruby"))
-    fmt.Println(strings.Index(cadena, "Go"))
-    fmt.Println(strings.Join([]string{"Android", "MATLAB", "MPLAB"}, "-"))
-    fmt.Println(strings.Repeat("Go ", 4))
-    fmt.Println(strings.Replace(cadena, "Python", "PHP", 1))
-    fmt.Println(strings.Split(cadena, "-"))
-    fmt.Println(strings.ToLower(cadena))
-    fmt.Println(strings.ToUpper(cadena))
-
-    b := []byte("go")
-    fmt.Println(b)
-
-    s := string([]byte{'g', 'o'})
-    fmt.Println(s)
+    archivo.WriteString("Hola mundo!")
 }
 
