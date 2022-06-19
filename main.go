5cb9f64906fd61e2c6d247e04b3cba4b8e1828c5
ian16munoz3nunez1 <ianemn1603@gmail.com>
2022-06-19 13:45:43 -0500

    2022-06-19_13:45 --> Estructuras/Interfaces

--- main.go
+++ main.go
@@ -1,28 +1,29 @@
 package main
 
 import (
-    "encoding/json"
     "fmt"
+    "math"
 )
 
+type Circulo struct {
+    radio float64
+}
+
+func (circulo *Circulo) area() float64 {
+    return circulo.radio * circulo.radio * math.Pi
+}
+
 func main() {
-    materias := map[string]map[string]int {
-        "Semestre 1": {
-            "Programacion": 100,
-            "Logica": 90,
-        },
-        "Semestre 2": {
-            "Algoritmia": 95,
-            "Calculo": 90,
-        },
-    }
+    c01 := Circulo{}
+    c02 := Circulo{radio: 16}
+    c03 := Circulo{8}
+    c04 := new(Circulo)
+    c05 := &Circulo{4}
 
-    fmt.Println(materias)
-
-    j, err := json.Marshal(materias)
-
-    fmt.Println(err)
-    fmt.Println(j)
-    fmt.Println(string(j))
+    fmt.Println(c01, c01.area())
+    fmt.Println(c02, c02.area())
+    fmt.Println(c03, c03.area())
+    fmt.Println(c04, c04.area())
+    fmt.Println(c05, c05.area())
 }
 
